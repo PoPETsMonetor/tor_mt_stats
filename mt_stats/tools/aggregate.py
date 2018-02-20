b8d142416f7e5cf71f1c853b258e4502b48719cd
Thien-Nam Dinh <thiennam.tnd@gmail.com>
2018-02-20 11:53:08 -0600

    python aggregator scrypt finished

--- mt_stats/tools/aggregate.py
+++ mt_stats/tools/aggregate.py
@@ -11,26 +11,27 @@ for filename in os.listdir(directory):
     # read in current aggregate file if it exists
     try:
         with open('../aggregate/' + group, 'rb') as csvfile:
-            reader = csv.reader(csvfile, skipiInitialspace=True, delimiter=',');
+            reader = csv.reader(csvfile, skipinitialspace=True, delimiter=',');
             aggregateTimeProfiles = [int(x) for x in next(reader)];
             aggregateTotalCounts = [float(x) for x in next(reader)];
             aggregateTimeStdevs = [float(x) for x in next(reader)];
-    except IOERROR:
+    except IOError:
         aggregateTimeProfiles = []
         aggregateTotalCounts = []
         aggregateTimeStdevs = []
 
     # open each new published file
     with open(directory + '/' + filename, 'rb') as csvfile:
-        reader = csv.reader(csvfile, skipInitialspace=True, delimiter=',')
+        reader = csv.reader(csvfile, skipinitialspace=True, delimiter=',')
         timeProfiles = [int(x) for x in next(reader)];
         totalCounts = [float(x) for x in next(reader)];
         timeStdevs = [float(x) for x in next(reader)];
 
     # add new published results to the aggregate
-    for i in range(len(aggegateTimeProfiles)):
+    for i in range(len(aggregateTimeProfiles), len(timeProfiles)):
+        aggregateTimeProfiles += [0]
+    for i in range(len(timeProfiles)):
         aggregateTimeProfiles[i] += timeProfiles[i]
-    aggregateTimeProfiles += timeProfiles[len(aggegateTimeProfiles):]
     aggregateTotalCounts += totalCounts
     aggregateTimeStdevs += timeStdevs
 
